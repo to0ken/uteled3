3e11f24f7cce68e39c8b66b482bb793e6a9409f8
to0ken <evgeniamafinova@gmail.com>
2026-09-10 11:22:48 +0300

    Create AppHader.vue

---
+++ src/components/AppHader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+defineProps<{
+  status: string;
+}>();
+</script>
+
+<template>
+  <header class="header">
+    <div>
+      <h1>
+        super messenger
+      </h1>
+      <p> {{status}}</p>
+    </div>
+    <span class="badge">
+        локально
+      </span>
+  </header>
+</template>
+
+<style scoped>
+
+.header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 18px 24px;
+  border-bottom: blueviolet;
+}
+
+.header h1{
+  margin: 0;
+  font-size: 10px;
+}
+
+.header p{
+  margin: 4px 0 0;
+  font-size: 10px;
+  color: darkgreen;
+}
+
+
+</style>
