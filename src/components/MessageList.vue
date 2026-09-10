4e2474412195e3b1e1509f7e51f6a74de799679f
to0ken <evgeniamafinova@gmail.com>
2026-09-10 11:25:26 +0300

    Create MessageList.vue

---
+++ src/components/MessageList.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+
+
+import type {Message} from "../types/message.ts";
+import MessageBubble from "./MessageBubble.vue";
+
+defineProps<{
+  messages:Message[];
+}>()
+</script>
+
+<template>
+  <main class="app">
+
+
+    <section class="chat">
+      <div class="chat-info">
+        <h2>первый чат</h2>
+        <p> ваш 1 лок мессенджер</p>
+      </div>
+      <div class="messages">
+        <div v-if="messages.length === 0"
+             class="empty">
+
+
+          <strong>
+            тут пусто
+          </strong>
+
+          <span>напишите первое соо</span>
+
+        </div>
+
+
+      </div>
+
+      <MessageBubble
+        v-for="message in messages"
+        :key="message.id"
+        :message="message"
+      />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.messages{
+  flex:1;
+  overflow-y:auto ;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 24px
+;
+}
+
+.empty{
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  text-align: center;
+  color: hotpink;
+}
+</style>
